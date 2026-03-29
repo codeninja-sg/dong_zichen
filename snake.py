8ecfc4dee2f7c76a434b605455248bed83620125
code_ninja <instructor@codeninja.sg>
2026-03-29 13:50:08 +0800

    Update snake.py

--- snake.py
+++ snake.py
@@ -25,8 +25,54 @@ def draw():
     canvas.create_rectangle(fx*SIZE, fy*SIZE, fx*SIZE+SIZE, fy*SIZE+SIZE,fill="red")
 
     for (x, y) in snake:
-        canvas.create_rectangle(x*SIZE, y*SIZE, x*SIZE+SIZE, y*SIZE+SIZE,fill="blue")
+        canvas.create_rectangle(x*SIZE, y*SIZE, x*SIZE+SIZE, y*SIZE+SIZE,fill="green")
 
-draw()
+
+def game_loop():
+    global snake, food
+    
+    head_x, head_y = snake[0]
+    new_head = (head_x + dx, head_y + dy)
+
+    snake.insert(0, new_head)
+    if new_head == food:
+
+        food = (random.randint(0, W//SIZE - 1),random.randint(0, H//SIZE - 1))
+
+    else:
+        snake.pop()
+
+    draw()
+    root.after(150, game_loop)
+
+   
+
+def up(event):
+    global dx, dy
+    dx, dy = 0, -1
+
+def down(event):
+    global dx, dy
+    dx, dy = 0, 1
+
+def left(event):
+    global dx, dy
+    dx, dy = -1, 0
+
+def right(event):
+    global dx, dy
+    dx, dy = 1, 0
+
+root.bind("<Up>", up)
+root.bind("<Down>", down)
+root.bind("<Left>", left)
+root.bind("<Right>", right)
+
+# restart_btn = tk.Button(root, text="Restart",command=restart)
+# restart_btn.pack(pady=5)
+
+
+draw() 
+root.after(150, game_loop)
 root.mainloop()
         
